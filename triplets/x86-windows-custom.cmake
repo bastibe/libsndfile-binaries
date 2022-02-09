2ab7785033ac3b6d0144424ac7f5b4b86138e1bd
Kyle Benesch <4b796c65+github@gmail.com>
2022-02-09 12:53:17 -0800

    Build and upload Windows libs.

---
+++ triplets/x86-windows-custom.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_PLATFORM_TOOLSET v140)
+set(VCPKG_BUILD_TYPE release)
+
+if (PORT STREQUAL libsndfile)
+	set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
