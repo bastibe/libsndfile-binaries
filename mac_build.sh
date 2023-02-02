ddd7480cfa9c72a6b946b7a9150087fffaba7e50
Joe Todd <joe.todd@sonos.com>
2023-02-02 17:07:21 +0000

    Try build libsndfile 1.2.0

--- mac_build.sh
+++ mac_build.sh
@@ -4,7 +4,7 @@ FLACVERSION=1.3.3
 OPUSVERSION=1.3.1
 MPG123VERSION=1.30.2
 LAMEVERSION=3.100
-SNDFILE_VERSION=1.1.0
+SNDFILE_VERSION=1.2.0
 
 JOBS=8
 SNDFILENAME=libsndfile-$SNDFILE_VERSION
@@ -14,16 +14,13 @@ OGG_LIBDIR="$(pwd)/libogg-$OGGVERSION/src/.libs"
 if [ "$1" = "arm64" ]; then
     echo "Cross compiling for Darwin arm64.."
     export MACOSX_DEPLOYMENT_TARGET=11.0
-
-    BUILD_HOST="--host=aarch64-apple-darwin"
-    XCORR_FLAGS="-arch arm64 -target arm64-apple-macos11"
-
-    export CFLAGS=$XCORR_FLAGS
-    export CXXFLAGS=$XCORR_FLAGS
+    BUILD_HOST="--host=aarch64-apple-darwin --target=arm64-apple-macos11"
+    EXTRA_CFLAGS="-arch arm64 -target arm64-apple-macos11"
 else
     echo "Building for Darwin $(uname -m).."
     export MACOSX_DEPLOYMENT_TARGET=10.9
     BUILD_HOST=""
+    EXTRA_CFLAGS=""
 fi
 
 # libogg
@@ -31,7 +28,7 @@ fi
 curl -LO https://downloads.xiph.org/releases/ogg/libogg-$OGGVERSION.tar.gz
 tar zxvf libogg-$OGGVERSION.tar.gz
 cd libogg-$OGGVERSION
-./configure $BUILD_HOST --disable-shared
+CFLAGS=$EXTRA_CFLAGS CXXFLAGS=$EXTRA_CFLAGS ./configure $BUILD_HOST --disable-shared
 make -j$JOBS
 cd ..
 
@@ -40,7 +37,7 @@ cd ..
 curl -LO https://downloads.xiph.org/releases/vorbis/libvorbis-$VORBISVERSION.tar.gz
 tar zxvf libvorbis-$VORBISVERSION.tar.gz
 cd libvorbis-$VORBISVERSION
-./configure $BUILD_HOST --disable-shared --with-ogg-includes=$OGG_INCDIR --with-ogg-libraries=$OGG_LIBDIR
+CFLAGS=$EXTRA_CFLAGS CXXFLAGS=$EXTRA_CFLAGS ./configure $BUILD_HOST --disable-shared --with-ogg-includes=$OGG_INCDIR --with-ogg-libraries=$OGG_LIBDIR
 make -j$JOBS
 cd ..
 
@@ -50,7 +47,7 @@ curl -LO https://downloads.xiph.org/releases/flac/flac-$FLACVERSION.tar.xz
 unxz flac-$FLACVERSION.tar.xz
 tar zxvf flac-$FLACVERSION.tar
 cd flac-$FLACVERSION
-./configure $BUILD_HOST --enable-static --disable-shared --with-ogg-includes=$OGG_INCDIR --with-ogg-libraries=$OGG_LIBDIR
+CFLAGS=$EXTRA_CFLAGS CXXFLAGS=$EXTRA_CFLAGS ./configure $BUILD_HOST --enable-static --disable-shared --with-ogg-includes=$OGG_INCDIR --with-ogg-libraries=$OGG_LIBDIR
 make -j$JOBS
 cd ..
 
@@ -59,7 +56,7 @@ cd ..
 curl -LO https://archive.mozilla.org/pub/opus/opus-$OPUSVERSION.tar.gz
 tar zxvf opus-$OPUSVERSION.tar.gz
 cd opus-$OPUSVERSION
-./configure $BUILD_HOST --disable-shared
+CFLAGS=$EXTRA_CFLAGS CXXFLAGS=$EXTRA_CFLAGS ./configure $BUILD_HOST --disable-shared
 make -j$JOBS
 cd ..
 
@@ -68,7 +65,7 @@ cd ..
 curl -LO https://sourceforge.net/projects/mpg123/files/mpg123/$MPG123VERSION/mpg123-$MPG123VERSION.tar.bz2
 tar zxvf mpg123-$MPG123VERSION.tar.bz2
 cd mpg123-$MPG123VERSION
-./configure $BUILD_HOST --enable-static --disable-shared
+CFLAGS=$EXTRA_CFLAGS CXXFLAGS=$EXTRA_CFLAGS ./configure $BUILD_HOST --enable-static --disable-shared
 make -j$JOBS
 cd ..
 
@@ -77,7 +74,7 @@ cd ..
 curl -LO https://sourceforge.net/projects/lame/files/lame/$LAMEVERSION/lame-$LAMEVERSION.tar.gz
 tar zxvf lame-$LAMEVERSION.tar.gz
 cd lame-$LAMEVERSION
-./configure $BUILD_HOST --enable-static --disable-shared
+CFLAGS=$EXTRA_CFLAGS CXXFLAGS=$EXTRA_CFLAGS ./configure $BUILD_HOST --enable-static --disable-shared
 make -j$JOBS
 cd ..
 
@@ -93,17 +90,22 @@ export VORBISENC_CFLAGS="-I$(pwd)/libvorbis-$VORBISVERSION/include"
 export VORBISENC_LIBS="$(pwd)/libvorbis-$VORBISVERSION/lib/libvorbisenc.la"
 export OPUS_CFLAGS="-I$(pwd)/opus-$OPUSVERSION/include"
 export OPUS_LIBS="$(pwd)/opus-$OPUSVERSION/libopus.la"
-export LAME_CFLAGS="-I$(pwd)/lame-$LAMEVERSION/include"
-export LAME_LIBS="$(pwd)/lame-$LAMEVERSION/libmp3lame/.libs/libmp3lame.la"
-export MPG123_CFLAGS="-I$(pwd)/mpg123-$MPG123VERSION/src/libmpg123 $LAME_CFLAGS"
-export MPG123_LIBS="$(pwd)/mpg123-$MPG123VERSION/src/libmpg123/libmpg123.la $LAME_LIBS"
+export MPEG_CFLAGS="-I$(pwd)/lame-$LAMEVERSION/include"
+export MPEG_LIBS="$(pwd)/lame-$LAMEVERSION/libmp3lame/.libs/libmp3lame.la"
+export MPG123_CFLAGS="-I$(pwd)/mpg123-$MPG123VERSION/src/libmpg123 $MPEG_CFLAGS"
+export MPG123_LIBS="$(pwd)/mpg123-$MPG123VERSION/src/libmpg123/libmpg123.la $MPEG_LIBS"
 
 curl -LO https://github.com/libsndfile/libsndfile/releases/download/$SNDFILE_VERSION/libsndfile-$SNDFILE_VERSION.tar.xz
 tar jxvf libsndfile-$SNDFILE_VERSION.tar.xz
 cd $SNDFILENAME
-./configure $BUILD_HOST --disable-static --disable-sqlite --disable-alsa
+./configure $BUILD_HOST --disable-static --disable-sqlite --disable-alsa --disable-full-suite
 make -j$JOBS
 cd ..
 
+if [ "$1" = "arm64" ]; then
+cp $SNDFILENAME/src/.libs/libsndfile.1.dylib libsndfile_arm64.dylib
+chmod -x libsndfile_arm64.dylib
+else
 cp $SNDFILENAME/src/.libs/libsndfile.1.dylib libsndfile.dylib
 chmod -x libsndfile.dylib
+fi
